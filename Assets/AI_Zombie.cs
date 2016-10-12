8bc777e2aa8460d56d1417e43c688ade0126d38c
LAPTOP-Stef <stef.baeyens@gmail.com>
2016-10-12 15:19:45 +0200

    AI - Zombie Added

---
+++ Assets/AI_Zombie.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_Zombie : MonoBehaviour {
+
+	public Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+
+	private Transform myTransfrom;
+
+	void Awake(){
+		myTransfrom = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag ("Player");
+	
+		target = go.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.DrawLine (target.position, myTransfrom.position, Color.yellow);
+
+		myTransfrom.rotation = Quaternion.Slerp (myTransfrom.rotation, Quaternion.LookRotation (target.position - myTransfrom.position), rotationSpeed * Time.deltaTime);
+
+		myTransfrom.position += myTransfrom.forward * moveSpeed * Time.deltaTime;
+	
+	}
+}
