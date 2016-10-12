fc1a7ccb2e9b58dd8a67e05cc355efbcba5814d9
LAPTOP-Stef <stef.baeyens@gmail.com>
2016-10-12 20:09:02 +0200

    12/10/2016 20:08 - Stef LAPTOP
    - Tweaked zombie AI a bit
    - Added destroy object to shell prefab in case of collision.

--- Assets/Scripts/ShellExplosion.cs
+++ Assets/Scripts/ShellExplosion.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class ShellExplosion : MonoBehaviour {
 
-	public GameObject explosion;
-	public Transform shell;
+	//public Transform shell;
+	//public Transform explosionPrefab;
 
+	void OnCollisionEnter(Collision collision){
+		//ContactPoint contact = collision.contacts [0];
+		//Quaternion rot = Quaternion.FromToRotation (Vector3.up, contact.normal);
+		//Vector3 pos = contact.point;
+		//Instantiate (explosionPrefab, pos, rot);
+		Destroy (gameObject);
+	}
 	// Use this for initialization
 	void Start () {
 	
