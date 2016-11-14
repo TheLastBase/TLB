aa117a93cd34af28ed7fe0593e6b1884de60db83
LAPTOP-Stef <stef.baeyens@gmail.com>
2016-11-14 22:20:01 +0100

    14/11/2016
    Onderandere zombie AI aangepast

--- Assets/Scripts/AI_Zombie.cs
+++ Assets/Scripts/AI_Zombie.cs
@@ -1,33 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class AI_Zombie : MonoBehaviour {
 
 	public Transform target;
 	public int moveSpeed;
 	public int rotationSpeed;
-
-	private Transform myTransfrom;
-
-	void Awake(){
-		myTransfrom = transform;
-	}
+	public Animator animator;
+	private bool alive;
+	//public Collider hitbox;
+	//public Collider aliveCollider;
+	//public Collider deadCollider;
+	public Transform myTransfrom;
+	public Transform parent;
+	public Text counter;
 
 	// Use this for initialization
 	void Start () {
-		GameObject go = GameObject.FindGameObjectWithTag ("Player");
-	
-		target = go.transform;
+		counter = GetComponent<Text> ();
+		alive = true;
+		//hitbox.enabled = true;
+		//aliveCollider.enabled = false;
+		//deadCollider.enabled = false;
+		int counterInt = parent.childCount;
+		counter.text = (counterInt.ToString ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (alive == true) {
+			myTransfrom.rotation = Quaternion.Slerp (myTransfrom.rotation, Quaternion.LookRotation (target.position - myTransfrom.position), rotationSpeed * Time.deltaTime);
+			myTransfrom.position += myTransfrom.forward * moveSpeed * Time.deltaTime;
+			//aliveCollider.enabled = true;
+			//deadCollider.enabled = false;
+		}
+	}
 
-		Debug.DrawLine (target.position, myTransfrom.position, Color.yellow);
-
-		myTransfrom.rotation = Quaternion.Slerp (myTransfrom.rotation, Quaternion.LookRotation (target.position - myTransfrom.position), rotationSpeed * Time.deltaTime);
-
-		myTransfrom.position += myTransfrom.forward * moveSpeed * Time.deltaTime;
-	
+	void OnTriggerEnter (Collider other){
+		animator.SetTrigger ("Hit");
+		alive = false;
+		//aliveCollider.enabled = true;
+		//deadCollider.enabled = false;
 	}
 }
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -6,7 +6,10 @@ public class Turret : MonoBehaviour {
 	public int speed = 30;
 	public Camera turretCam;
 	public Camera mainCam;
-	public Collider turret;
+	public GameObject player;
+	public GameObject turret;
+	private bool insideTurret;
+	public GameObject exitpoint;
 
 	// Use this for initialization
 	void Start () {
@@ -26,10 +29,18 @@ public class Turret : MonoBehaviour {
 		if (Input.GetKey (KeyCode.O)) {
 			turretCam.enabled = true;
 			mainCam.enabled = false;
+			insideTurret = true;
+
 		}
 		if (Input.GetKey (KeyCode.U)) {
 			mainCam.enabled = true;
 			turretCam.enabled = false;
+			insideTurret = false;
+			player.transform.position = exitpoint.transform.position;
+			player.transform.rotation = exitpoint.transform.rotation;
+		}
+		if (insideTurret == true) {
+			player.transform.position = turret.transform.localPosition;
 		}
 	}
 }
